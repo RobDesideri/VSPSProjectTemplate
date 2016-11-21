6c1c935c30f846edaf9f8c7c0f3d074b4aa72412
Roberto Desideri <robertodesideri@outlook.com>
2016-11-21 20:07:55 +0100

    fix lib folder path

--- test/InstallTestingDepends.ps1
+++ test/InstallTestingDepends.ps1
@@ -4,4 +4,4 @@ if(-not $tp)
    New-Item -Path $PSScriptRoot -Name lib -ItemType Directory
 }
 Import-Module "$PSScriptRoot\..\lib\PSDepend"
-Invoke-PSDepend -Path $PSScriptRoot\TestDepends.psd1 -Target $PSScriptRoot\lib -Install
+Invoke-PSDepend -Path $PSScriptRoot\TestDepends.psd1 -Target $PSScriptRoot\lib -Install -Force
